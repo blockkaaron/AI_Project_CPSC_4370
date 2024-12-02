cdcf537bc847e4fdff48e0a7056f82bdc423599a
blockkaaron <blockkaaron@gmail.com>
2024-12-01 21:22:52 -0600

    Initial commit

--- project_package/projectmain.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-datainput = pd.read_csv('../creditcard.csv')
-
-print(datainput[0:5],"\n")
-
-print("Shape of Complete Data Set")
-print(datainput.shape,"\n")
--- project_package/testcsvconnection.py
+++ project_package/testcsvconnection.py
@@ -3,15 +3,19 @@ import pandas as pd
 '''
 This .py is to test your file structure to ensure you're ready to run main.py
 '''
-# attempt to test connection to data file
+# attempt to test connection to data files
 try:
-    # here, we establish our data or file path and read file
-    datainput = pd.read_csv('creditcard.csv')
+    # here, we establish our data files and read them in to confirm connection
+    datatest = pd.read_csv('test.csv')
+    datatrain = pd.read_csv('train.csv')
 
     # print first 10 rows to test
-    print(datainput[0:10],)
-    print(datainput.shape,"Total columns/rows read successfully")
+    print(datatest[0:10],)
+    print(datatest.shape,"Total columns/rows from test.csv read successfully")
+    print("\n")
+    print(datatrain[0:10],)
+    print(datatrain.shape,"Total columns/rows from train.csv read successfully")
 
-# catch exception
+# catch exceptions
 except:
-    print("Exception occurred, \nPlease check the creditcard.csv file location and ensure it is accessible.")
+    print("Exception occurred, \nPlease check the .CSV files, their locations, and ensure they are accessible.")
